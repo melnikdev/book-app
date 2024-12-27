9a3d5c8ead906d1b07854469597ac1156641b668
Serhii Melnik <melnikdev@gmail.com>
2024-12-27 11:51:07 +0200

    feat add migrations for book, author

---
+++ database/migrations/2024_12_27_090049_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->date('published_date');
+
+            $table->foreignId('author_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
---
+++ database/migrations/2024_12_27_093804_create_author_book_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('author_book', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('author_id')
+                ->nullable()
+                ->constrained('authors')
+                ->onUpdate('SET NULL')
+                ->onDelete('CASCADE');
+            $table->foreignId('book_id')
+                ->nullable()
+                ->constrained('books')
+                ->onUpdate('SET NULL')
+                ->onDelete('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('author_book');
+    }
+};
