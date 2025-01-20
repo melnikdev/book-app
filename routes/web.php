960b15d652189c7485170adcbf634b9559d001ab
Serhii Melnik <melnikdev@gmail.com>
2025-01-20 12:34:37 +0200

    Add route for Filament export downloads
    Introduce a new route to handle export downloads in Filament. This route is protected by the 'web' and 'auth:web' middleware to ensure secure access.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Filament\Actions\Exports\Http\Controllers\DownloadExport;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/filament/exports/{export}/download', DownloadExport::class)
+    ->name('filament.exports.download')
+    ->middleware(['web', 'auth:web']);
