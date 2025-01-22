2d181b66554a24732f41b4c526431655f9210414
Serhii Melnik <melnikdev@gmail.com>
2025-01-22 10:57:45 +0200

    Add factories and enable HasFactory for Author and Book models
    This commit introduces `AuthorFactory` and `BookFactory` to streamline test data generation.

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,15 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Book extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'title',
         'published_date',
     ];
+
     public function authors(): BelongsToMany
     {
         return $this->belongsToMany(Author::class);
